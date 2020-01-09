c75a43c67b5e958f2611fd7ba7e786d05f4d1bf6
王黄鹏 <245232172@qq.com>
2020-01-09 10:44:29 +0800

    密码修改

--- src/main/java/com/sinosoft/demo/domain/util/FtpUtil.java
+++ src/main/java/com/sinosoft/demo/domain/util/FtpUtil.java
@@ -1,9 +1,7 @@
 package com.sinosoft.demo.domain.util;
 
 import com.jcraft.jsch.*;
-import com.sinosoft.demo.domain.entity.FtpEntity;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
